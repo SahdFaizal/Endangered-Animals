24b78a73b21fc83a0a86636d7fcfa89900cc2d95
SandeedAli090610 <110333105+SandeedAli090610@users.noreply.github.com>
2022-09-14 18:35:04 +0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,10 @@
         var fullImgBox = document.getElementById("fullImgBox");
         var fullImg = document.getElementById("fullImg");
-
+        var link = document.createElement("link");
+        link.rel = "stylesheet";
+        link.type = "text/css";
+        link.href = "style.css";
+       
         function openFullImg(pic){
             fullImgBox.style.display = "flex";
             fullImg.src = pic;
