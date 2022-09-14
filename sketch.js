5c077fd0c98a942cf272db4dfe013be1da6e7b9b
SahdFaizal <113359217+SahdFaizal@users.noreply.github.com>
2022-09-14 20:42:09 +0400

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,9 +1,9 @@
-        var fullImgBox = document.getElementById("fullImgBox");
-        var fullImg = document.getElementById("fullImg");
         var link = document.createElement("link");
         link.rel = "stylesheet";
         link.type = "text/css";
         link.href = "style.css";
+        var fullImgBox = document.getElementById("fullImgBox");
+        var fullImg = document.getElementById("fullImg");
        
         function openFullImg(pic){
             fullImgBox.style.display = "flex";
