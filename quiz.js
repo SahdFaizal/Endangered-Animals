abb35a3661f45f50a3654fac84ed45799549ab6a
SahdFaizal <113359217+SahdFaizal@users.noreply.github.com>
2022-10-01 15:36:40 +0400

    Update quiz.js

--- quiz.js
+++ quiz.js
@@ -15,7 +15,7 @@ nextButton.addEventListener('click', () => {
 
 function startGame() {
   startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .10)
+  shuffledQuestions = questions.sort(() => Math.random() - .5)
   currentQuestionIndex = 0
   questionContainerElement.classList.remove('hide')
   setNextQuestion()
